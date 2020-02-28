95ad3bdf23e2034ae19cd524ad77a56c0e53cb2c
mjcarter <mjcarter@ualberta.ca>
2020-02-28 11:18:36 -0700

    started grapheme.py

---
+++ grapheme.py
@@ -0,0 +1,112 @@
+## Michael Carter
+## mjcarter@ualberta.ca
+## February/March 2020
+
+## Objects of this class represent a single grapheme (a letter or number), 
+## which will be presented on the main game board. 
+## This is done so that colours can be easily changed and turned on/off.
+## This class is based on my class Word from Synesthetic-Word-Presenter:
+##   https://github.com/m-j-carter/Synesthetic-Word-Presenter
+
+## Class utilizes methods in the uagame library
+
+# Notes/Changelog:
+#   - color of the printed grapheme is currently determined at calltime
+#       by the calling function. This could get changed to an object attribute
+#       that would need to be set at time of creation, or otherwise. 
+
+
+import pygame
+
+DEFAULT_FONT_COLOR = 'black'
+DEFAULT_FONT_SIZE = 72
+DEFAULT_FONT_NAME = 'arial'
+
+pygame.init()
+
+class Grapheme:
+    """ Represents a grapheme object (letter/number), which is assigned a corresponding colour,
+        as stated in SYN_COLORS. draw_grapheme() can be called with the boolean attribute
+        is_colored, which draws it with either those colors or the DEFAULT_FONT_COLOR.
+    """
+
+    """
+    # Colour Correspondences:
+    # Based on the modal results from Witthoft, Winawer, and Eagleman (2015)
+    "A":"red",
+    "B":"blue",
+    "C":"yellow",
+    "D":"blue",
+    "E":"green",
+    "F":"green",
+    "G":"green",
+    "H":"orange",
+    "I":"white",
+    "J":"orange",
+    "K":"orange",
+    "L":"yellow",
+    "M":"red",
+    "N":"orange",
+    "O":"white",
+    "P":"purple",
+    "Q":"purple",
+    "R":"red",
+    "S":"yellow",
+    "T":"blue",
+    "U":"orange",
+    "V":"purple",
+    "W":"blue",
+    "X":"black",
+    "Y":"yellow",
+    "Z":"black",
+    """    
+
+    SYN_COLORS = {
+        "A":"red",
+        "B":"blue",
+        "E":"green",
+        "G":"green",
+        "L":"yellow",
+        "N":"orange",
+        "Y":"yellow"
+    }
+
+    @classmethod
+    def set_window(cls,window_from_parent):
+        cls.window = window_from_parent
+
+
+    def __init__(self, char):
+        self.__char = str(char)        # self.__char is a string of len 1 (python doesn't have char types)
+        self.__color = DEFAULT_FONT_COLOR         # initialize as default color
+        self.__generate_color()
+
+    def __str__(self):
+        return str(self.__char)
+
+    def __repr__(self):
+        return str(self.__char)    
+
+    def __generate_color(self):
+        """ If the grapheme is in SYN_COLORS, this generates 
+            and assigns the synesthetic color to the grapheme. """
+
+        temp = Grapheme.SYN_COLORS.get(str(self.__char).upper())
+        if temp:
+            self.__color = Grapheme.SYN_COLORS.get(str(self.__char).upper())     
+
+    def draw_grapheme(self, x, y, is_colored=False):
+        """ draws the character to the display using modules 
+            in the uagame library. """
+        # Grapheme.window.set_font_name(DEFAULT_FONT_NAME)
+        # Grapheme.window.set_font_size(DEFAULT_FONT_SIZE)
+        if is_colored:
+            Grapheme.window.set_font_color(self.letter_colors[index])   
+        else:
+            Grapheme.window.set_font_color(DEFAULT_FONT_COLOR)   
+
+        Grapheme.window.draw_string(self.__char, x, y)        
+
+    def get_grapheme(self):
+        """ returns the grapheme character """
+        return self.__char
